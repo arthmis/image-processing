3f75d5ab9aa626bfa12f2678391ce7d49d39086e
lloyd <lmassiah37@gmail.com>
2018-12-02 00:08:05 -0500

    added a function to draw a grayscale histogram on a white background with a modifiable image width for the histogram canvas

--- examples/histograms.rs
+++ examples/histograms.rs
@@ -1,23 +1,26 @@
 extern crate image;
 extern crate image_processing;
 
-use image::{ImageBuffer, RgbaImage, GrayAlphaImage, ConvertBuffer, LumaA, Pixel, GrayImage, GenericImage};
+use image::{
+    GrayImage, 
+};
 
 use image_processing::statistics::histogram::*;
 
 fn main() {
-
-    // let img = image::open("images/london-bridge.jpg")
-        // .expect("Image not found").to_luma();
+    let img = image::open("images/london-bridge.jpg")
+       .expect("Image not found").to_luma();
 
     // let some = integral_image(&img); 
 
-    let my_img = GrayImage::from_raw(
-        4, 
-        3, 
-        vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    ).unwrap(); 
+    // let my_img = GrayImage::from_raw(
+    //     4, 
+    //     3, 
+    //     vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    // ).unwrap(); 
+    //
+    let img_hist = gray_histogram(&img);
+    draw_histogram(256, 256, "test2.png", &img_hist);
+    draw_histogram(512, 256, "test.png", &img_hist);
 
-    gray_histogram(&my_img);
- 
-}
+} 
--- src/statistics/histogram.rs
+++ src/statistics/histogram.rs
@@ -1,4 +1,4 @@
-use image::{RgbaImage, GrayAlphaImage, GrayImage};
+use image::{RgbaImage, GrayAlphaImage, GrayImage, ImageBuffer, Luma};
 
 pub struct RgbHistogram {
     pub red: [u32; 256],
@@ -32,6 +32,71 @@ pub fn cumulative_gray_histogram(
         cum_histogram.values[i] = sum;
     }
     cum_histogram
+}
+
+pub fn draw_histogram(image_width: u32, image_height: u32, file: &str, gray_hist: &GrayHistogram) {
+    let hist_color = {
+        let pixel = [90; 1];
+        let pixel = Luma(pixel); 
+        pixel
+    };
+    let max_hist_val = {
+        let mut max = 0;
+        for i in 0..255 {
+            if gray_hist.values[i] > max {
+                max = gray_hist.values[i];
+            }
+        }
+        max
+    };
+
+    let draw_hist = || {    let raw_image_buffer = vec![255; (image_width * image_height) as usize];
+    let mut image: GrayImage = ImageBuffer::from_raw(
+        image_width, image_height, raw_image_buffer)
+            .expect("white image could not be created"); 
+    let step = image_width / 256;
+    for i in 0..255 {
+        let mut y = 255 * gray_hist.values[i] / max_hist_val;
+        y = 255 - y; 
+        image.put_pixel(i as u32, y as u32, hist_color);
+        for n in y..255 {
+            image.put_pixel(i as u32, n as u32, hist_color);
+        } 
+    }
+    image.save(file).expect("couldn't save png image");
+    };
+
+    // draw_hist(); 
+    
+    let raw_image_buffer = vec![255; (image_width * image_height) as usize];
+    let mut image: GrayImage = ImageBuffer::from_raw(
+        image_width, image_height, raw_image_buffer)
+            .expect("white image could not be created"); 
+    let step = image_width / 256;
+    println!("{}", step);
+    let hist_width = step * 256; 
+    let hist_end = image_width - (image_width - hist_width) / 2;
+    let hist_begin = 0 + (image_width - hist_width) / 2;  
+    for i in 0..255 {
+        let mut y = 255 * gray_hist.values[i] / max_hist_val;
+        y = 255 - y; 
+        let x_pos = (i as u32) * step + hist_begin;
+        for s in 0..step {
+            let x = x_pos + s as u32;
+            image.put_pixel(x, y, hist_color);
+            println!("{} {} {}", x, s, y);
+            for n in y..255 {
+                image.put_pixel(x, n as u32, hist_color);
+            } 
+         }
+        // image.put_pixel(i as u32, y as u32, hist_color);
+        // for n in y..255 {
+        //     image.put_pixel(i as u32, n as u32, hist_color);
+        // } 
+    }
+    image.save(file).expect("couldn't save png image");
+    
+
 }
 
 pub fn cumulative_rgb_histogram(rgb_hist: &RgbHistogram) -> CumuRgbHistogram {
