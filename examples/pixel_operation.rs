aca2e188e57c433c8ca45d8a02bb2bca88b49c8a
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-01-13 17:54:34 -0500

    finished changing to 2018, i think

--- examples/pixel_operation.rs
+++ examples/pixel_operation.rs
@@ -1,16 +1,16 @@
 extern crate image_processing;
 extern crate image;
 
-use image_processing::point_operations::invert_grayscale;
+use image_processing::pixel_operations::invert_grayscale;
+use image_processing::window::display_image;
 
 use image::ConvertBuffer;
-use image_processing::window::display_image;
 
 fn main() {
     let img = image::open("images/gray-london-bridge.jpg")
         .expect("couldn't find image at that path")
         .to_luma_alpha();
     let inverted_img = invert_grayscale(&img);
-    display_image("inverted", inverted_img.convert(), 1000, 1000);
+    display_image("inverted", &inverted_img.convert(), 1000, 1000);
 
 }
