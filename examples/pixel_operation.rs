7d5c12be5b912c06e98817e1f21f931c32984b03
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-01-13 19:28:28 -0500

    added manual thresholding functions

--- examples/pixel_operation.rs
+++ examples/pixel_operation.rs
@@ -10,9 +10,15 @@ fn main() {
     let img = image::open("images/gray-london-bridge.jpg")
         .expect("couldn't find image at that path")
         .to_luma_alpha();
-    let inverted_img = invert_grayscale(&img);
-    display_image("inverted", &inverted_img.convert(), 1000, 1000);
-    let inverted_img = invert_grayscale_mut(img);
-    display_image("inverted", &inverted_img.convert(), 1000, 1000);
+    let width = 1000;
+    let height = 1000;
+    // let inverted_img = invert_grayscale(&img);
+    // display_image("inverted", &inverted_img.convert(), 1000, 1000);
+    // let inverted_img = invert_grayscale_mut(img);
+    // display_image("inverted", &inverted_img.convert(), 1000, 1000);
+    let thresholded_image = threshold(&img, 125);
+    display_image("thresholded", &thresholded_image.convert(), width, height);
+    let thresholded_image = threshold_mut(img, 125);
+    display_image("thresholded mut", &thresholded_image.convert(), width, height);
 
 }
--- src/pixel_operations.rs
+++ src/pixel_operations.rs
@@ -3,6 +3,7 @@
 use image::{GrayAlphaImage, LumaA};
 
 const MAX_VALUE: u8 = 255;
+const MIN_VALUE: u8 = 0;
 
 /// inverts image in place
 pub fn invert_grayscale_mut(mut image: GrayAlphaImage) -> GrayAlphaImage {
@@ -14,9 +15,7 @@ pub fn invert_grayscale_mut(mut image: GrayAlphaImage) -> GrayAlphaImage {
     //     }
     // }
     for pixel in image.enumerate_pixels_mut() {
-        let inverted_pixel = LumaA([MAX_VALUE - pixel.2.data[0 as usize], MAX_VALUE]);
         pixel.2.data[0 as usize] = MAX_VALUE - pixel.2.data[0 as usize];
-        pixel.2.data[1 as usize] = MAX_VALUE;
     }
     image
 }
@@ -30,4 +29,32 @@ pub fn invert_grayscale(image: &GrayAlphaImage) -> GrayAlphaImage {
         inverted_image.put_pixel(pixel.0, pixel.1, inverted_pixel);
     }
     inverted_image
+}
+
+/// thresholds image in place
+pub fn threshold_mut(mut image: GrayAlphaImage, threshold: u8) -> GrayAlphaImage {
+    for pixel in image.enumerate_pixels_mut() {
+        if pixel.2.data[0 as usize] < threshold {
+            pixel.2.data[0 as usize] = MIN_VALUE;
+        } else {
+            pixel.2.data[0 as usize] = MAX_VALUE;
+        }
+            
+    } 
+    image
+}
+
+/// thresholds image
+pub fn threshold(image: &GrayAlphaImage, threshold: u8) -> GrayAlphaImage {
+    let mut threshold_image = image.clone();
+    let white = LumaA([MAX_VALUE, MAX_VALUE]);
+    let black = LumaA([MIN_VALUE, MAX_VALUE]);
+    for pixel in image.enumerate_pixels() {
+        if pixel.2.data[0 as usize] < threshold {
+            threshold_image.put_pixel(pixel.0, pixel.1, black);
+        } else {
+            threshold_image.put_pixel(pixel.0, pixel.1, white);
+        }
+    }
+    threshold_image
 }
