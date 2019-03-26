69cc79264094b886f60abed2d42903b043c8a835
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-03-26 13:21:55 -0400

    handled one corner case for 3x3 box filter

--- examples/filters.rs
+++ examples/filters.rs
@@ -3,11 +3,26 @@ use image_processing;
 
 fn main() {
 
-    let image = image::open("images/base.jpg").expect("shell not in correct directory").to_luma();
+    // let image = image::open("images/base.jpg").expect("shell not in correct directory").to_luma();
+    // let raw_buffer = vec![
+    //     5,  6,  7,  2, 30,
+    //     83, 23, 53, 2,  3,
+    //     5,  5,  5,  5,  5,
+    //     34, 50, 29, 13, 63,
+    //     39, 1,  93, 86, 52
+    // ];
+    let raw_buffer = vec![
+        5,  6,  7,
+        83, 23, 53,
+        5,  5,  5,
+        // 34, 50, 29,
+        // 39, 1,  93,
+    ];
+    let image: image::GrayImage = image::ImageBuffer::from_raw(5, 5, raw_buffer).unwrap();
     let box_filter_image = image_processing::filters::box_3x3(&image);
 
     use image_processing::window;
     use image::ConvertBuffer;
 
-    window::display_multiple_images(&["image", "box filter image"], &[&image.convert(), &box_filter_image.convert()], 500, 500);
+    // window::display_multiple_images(&["image", "box filter image"], &[&image.convert(), &box_filter_image.convert()], 500, 500);
 }
--- src/filters.rs
+++ src/filters.rs
@@ -1,14 +1,9 @@
-use image::{GrayImage};
+use image::GrayImage;
 
 pub fn box_3x3(image: &GrayImage) -> GrayImage {
     let mut new_image = image.clone();
     // let filter: [[u32; 3]; 3] = [[1; 3]; 3];
-    let filter =
-    [
-        [1, 1, 1],
-        [1, 1, 1],
-        [1, 1, 1]
-    ];
+    let filter = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
 
     let image_height = image.height();
     let image_width = image.width();
@@ -32,34 +27,50 @@ pub fn box_3x3(image: &GrayImage) -> GrayImage {
         let begin_x: isize = x as isize - (filter.len() / 2) as isize;
         let begin_y: isize = y as isize - (filter.len() / 2) as isize;
         let filter_len = filter.len() as isize;
+        let end_x = begin_x + 3;
+        let end_y = begin_y + 3;
+        println!("\nnext");
         println!("begin x: {}, begin y: {}", begin_x, begin_y);
-        // TODO build a small image maybe 5 x 5 to test out filters and make it easy to keep track
-        // of calculations
-        for j in begin_y..filter_len-1 {
-            for i in begin_x..filter_len-1 {
+        println!("end   x: {}, end   y: {}", end_x, end_y);
+        for j in begin_y..end_y {
+            for i in begin_x..end_x {
+                println!("i: {}, j: {}", i, j);
                 if i < 0 && j < 0 {
-                    println!("top left {}, {}, pixel: {}", i, j, image.get_pixel(x, y).data[0]);
-                     sum += image.get_pixel(x, y).data[0] as f64;
+                    println!(
+                        "top left        {}, {}, pixel: {}",
+                        i,
+                        j,
+                        image.get_pixel(x, y).data[0]
+                    );
+                    sum += image.get_pixel(x, y).data[0] as f64;
                 } else if j < 0 {
-                    println!("top left {}, {}, pixel: {}", i, j, image.get_pixel(i as u32, y).data[0]);
-                    sum += image.get_pixel(i as u32, y).data[0] as f64; 
+                    println!(
+                        "top             {}, {}, pixel: {}",
+                        i,
+                        j,
+                        image.get_pixel(i as u32, y).data[0]
+                    );
+                    sum += image.get_pixel(i as u32, y).data[0] as f64;
                 } else if i < 0 {
-                    println!("top left {}, {}, pixel: {}", i, j, image.get_pixel(x, j as u32).data[0]);
+                    println!(
+                        "left           {}, {}, pixel: {}",
+                        i,
+                        j,
+                        image.get_pixel(x, j as u32).data[0]
+                    );
                     sum += image.get_pixel(x, j as u32).data[0] as f64;
                 } else if false {
-                    
+
                 } else {
-                    println!("else block");
+                    println!("in image bounds {}, {}, pixel: {}", i, j, image.get_pixel(i as u32, j as u32).data[0]);
                     sum += image.get_pixel(i as u32, j as u32).data[0] as f64;
                 }
                 // println!("i: {}, j: {}", i, j);
-                
-
             }
         }
-        dbg!(sum);
-        dbg!(sum / 9.0);
-        dbg!((sum / 9.0).round());
+        // dbg!(sum);
+        // dbg!(sum / 9.0);
+        // dbg!((sum / 9.0).round());
         let average = (sum / 9.0).round() as u8;
 
         use image::Luma;
@@ -72,5 +83,4 @@ pub fn box_3x3(image: &GrayImage) -> GrayImage {
 
 pub fn box_3x3_mut(image: &mut GrayImage) {
     let new_image = image.clone();
-
 }
