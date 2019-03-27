07583353bd4577021a901d4deb021571b4876b5c
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-03-27 19:36:32 -0400

    added rest of corner cases for box filter, still need to test them

--- examples/filters.rs
+++ examples/filters.rs
@@ -1,5 +1,5 @@
-use image;
-use image_processing;
+use image::{self, ImageBuffer, GrayImage};
+use image_processing::filters::box_3x3;
 
 fn main() {
 
@@ -15,11 +15,9 @@ fn main() {
         5,  6,  7,
         83, 23, 53,
         5,  5,  5,
-        // 34, 50, 29,
-        // 39, 1,  93,
     ];
-    let image: image::GrayImage = image::ImageBuffer::from_raw(5, 5, raw_buffer).unwrap();
-    let box_filter_image = image_processing::filters::box_3x3(&image);
+    let image: GrayImage = ImageBuffer::from_raw(5, 5, raw_buffer).unwrap();
+    let box_filter_image = box_3x3(&image);
 
     use image_processing::window;
     use image::ConvertBuffer;
--- src/filters.rs
+++ src/filters.rs
@@ -2,7 +2,6 @@ use image::GrayImage;
 
 pub fn box_3x3(image: &GrayImage) -> GrayImage {
     let mut new_image = image.clone();
-    // let filter: [[u32; 3]; 3] = [[1; 3]; 3];
     let filter = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
 
     let image_height = image.height();
@@ -13,54 +12,66 @@ pub fn box_3x3(image: &GrayImage) -> GrayImage {
         let x = pixel.0;
         let y = pixel.1;
         let mut sum: f64 = 0.0;
-        // for j in 0..filter.len() {
-        //     for i in 0..filter.len() {
-        //         if x - (filter_center.0 - i) <= x {
-        //             sum += image.get_pixel(x + filter_center.0 - i, y).data[0];
-        //             sum += image.get_pixel(x - (filter.center.0 - i), y).data[0];
-        //         }
-        //         if y - (filter_center.1 - j) < y {
-        //             sum += y;
-        //         }
-        //     }
-        // }
+
         let begin_x: isize = x as isize - (filter.len() / 2) as isize;
         let begin_y: isize = y as isize - (filter.len() / 2) as isize;
-        let filter_len = filter.len() as isize;
         let end_x = begin_x + 3;
         let end_y = begin_y + 3;
+
+        let filter_len = filter.len() as isize;
         println!("\nnext");
         println!("begin x: {}, begin y: {}", begin_x, begin_y);
         println!("end   x: {}, end   y: {}", end_x, end_y);
+
         for j in begin_y..end_y {
             for i in begin_x..end_x {
                 println!("i: {}, j: {}", i, j);
-                if i < 0 && j < 0 {
+                if i < 0 && j < 0 { // top left
                     println!(
                         "top left        {}, {}, pixel: {}",
                         i,
                         j,
-                        image.get_pixel(x, y).data[0]
+                        image.get_pixel(0, 0).data[0]
                     );
-                    sum += image.get_pixel(x, y).data[0] as f64;
-                } else if j < 0 {
+                    sum += image.get_pixel(0, 0).data[0] as f64;
+                } else if j < 0 { // top
                     println!(
                         "top             {}, {}, pixel: {}",
                         i,
                         j,
-                        image.get_pixel(i as u32, y).data[0]
+                        image.get_pixel(i as u32, 0).data[0]
                     );
-                    sum += image.get_pixel(i as u32, y).data[0] as f64;
-                } else if i < 0 {
+                    sum += image.get_pixel(i as u32, 0).data[0] as f64;
+                } else if i < 0 { // left
                     println!(
                         "left           {}, {}, pixel: {}",
                         i,
                         j,
-                        image.get_pixel(x, j as u32).data[0]
+                        image.get_pixel(0, j as u32).data[0]
                     );
-                    sum += image.get_pixel(x, j as u32).data[0] as f64;
-                } else if false {
-
+                    sum += image.get_pixel(0, j as u32).data[0] as f64;
+                } else if i > image_width as isize - 1 && j < 0 { // top right
+                    println!(
+                        "left           {}, {}, pixel: {}",
+                        i,
+                        j,
+                        image.get_pixel(image_width - 1, 0).data[0]
+                    );
+                    sum += image.get_pixel(image_width - 1, 0).data[0] as f64;
+                } else if i > image_width as isize - 1 { // right
+                    println!(
+                        "left           {}, {}, pixel: {}",
+                        i,
+                        j,
+                        image.get_pixel(image_width - 1, j as u32).data[0]
+                    );
+                    sum += image.get_pixel(image_width - 1, j as u32).data[0] as f64;
+                } else if i < 0 && j > image_height as isize - 1 { // bottom left
+                    sum += image.get_pixel(0, image_height - 1).data[0] as f64;
+                } else if j > image_height as isize - 1 { // bottom
+                    sum += image.get_pixel(i as u32, image_height - 1).data[0] as f64;
+                } else if i > image_width as isize - 1 && j > image_height as isize - 1 { // bottom right
+                    sum += image.get_pixel(image_width - 1, image_height - 1).data[0] as f64;
                 } else {
                     println!("in image bounds {}, {}, pixel: {}", i, j, image.get_pixel(i as u32, j as u32).data[0]);
                     sum += image.get_pixel(i as u32, j as u32).data[0] as f64;
