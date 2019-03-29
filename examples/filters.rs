56cf3203326308ad40f42ad81e3dac0e98896c6b
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-03-29 14:03:05 -0400

    made a box function for generic size box filters

--- examples/filters.rs
+++ examples/filters.rs
@@ -4,13 +4,13 @@ use image_processing::filters::box_3x3;
 fn main() {
 
     let image = image::open("images/base.jpg").expect("shell not in correct directory").to_luma();
-    // let raw_buffer = vec![
-    //     5,  6,  7,
-    //     83, 23, 53,
-    //     8,  9,  10,
-    // ];
+    let raw_buffer = vec![
+        5,  6,  7,
+        83, 23, 53,
+        8,  9,  10,
+    ];
     // let image: GrayImage = ImageBuffer::from_raw(3, 3, raw_buffer).unwrap();
-    let box_filter_image = box_3x3(&image, 5, 5);
+    let box_filter_image = box_3x3(&image, 99, 99);
 
     use image_processing::window;
     use image::ConvertBuffer;
--- src/filters.rs
+++ src/filters.rs
@@ -1,12 +1,12 @@
 use image::GrayImage;
 
+/// If the filter width or height are not odd in size then they are made into odd lengths
+/// by subtracting 1
+/// If the width or height is less than 3 then the input image is returned without any convolution done 
 pub fn box_3x3(image: &GrayImage, filter_width: u32, filter_height: u32) -> GrayImage {
     let mut new_image = image.clone();
-    let filter = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
 
-    let image_height = image.height();
-    let image_width = image.width();
-
+    let (image_width, image_height) = image.dimensions();
 
     let filter_width: u32 = 
         if filter_width % 2 == 0 {
@@ -29,7 +29,7 @@ pub fn box_3x3(image: &GrayImage, filter_width: u32, filter_height: u32) -> Gray
     for pixel in image.enumerate_pixels() {
         let x = pixel.0;
         let y = pixel.1;
-        let mut sum: f64 = 0.0;
+        let mut sum: u32 = 0;
 
         let begin_x: isize = x as isize - (filter_width / 2) as isize;
         let begin_y: isize = y as isize - (filter_height / 2) as isize;
@@ -39,28 +39,28 @@ pub fn box_3x3(image: &GrayImage, filter_width: u32, filter_height: u32) -> Gray
         for j in begin_y..end_y {
             for i in begin_x..end_x {
                 if i < 0 && j < 0 { // top left
-                    sum += image.get_pixel(0, 0).data[0] as f64;
+                    sum += image.get_pixel(0, 0).data[0] as u32;
                 } else if i > (image_width - 1) as isize && j < 0 { // top right
-                    sum += image.get_pixel(image_width - 1, 0).data[0] as f64;
+                    sum += image.get_pixel(image_width - 1, 0).data[0] as u32;
                 } else if j < 0 { // top
-                    sum += image.get_pixel(i as u32, 0).data[0] as f64;
+                    sum += image.get_pixel(i as u32, 0).data[0] as u32;
                 } else if i < 0 && j > (image_height as isize) - 1 { // bottom left
-                    sum += image.get_pixel(0, image_height - 1).data[0] as f64;
+                    sum += image.get_pixel(0, image_height - 1).data[0] as u32;
                 } else if i > image_width as isize - 1 && j > image_height as isize - 1 { // bottom right
-                    sum += image.get_pixel(image_width - 1, image_height - 1).data[0] as f64;
+                    sum += image.get_pixel(image_width - 1, image_height - 1).data[0] as u32;
                 } else if i < 0 { // left
-                    sum += image.get_pixel(0, j as u32).data[0] as f64;
+                    sum += image.get_pixel(0, j as u32).data[0] as u32;
                 } else if i > image_width as isize - 1 { // right
-                    sum += image.get_pixel(image_width - 1, j as u32).data[0] as f64;
+                    sum += image.get_pixel(image_width - 1, j as u32).data[0] as u32;
                 } else if j > image_height as isize - 1 { // bottom
-                    sum += image.get_pixel(i as u32, image_height - 1).data[0] as f64;
+                    sum += image.get_pixel(i as u32, image_height - 1).data[0] as u32;
                 } else {
-                    sum += image.get_pixel(i as u32, j as u32).data[0] as f64;
+                    sum += image.get_pixel(i as u32, j as u32).data[0] as u32;
                 }
             }
         }
 
-        let average = (sum / 9.0).round() as u8;
+        let average = (sum as f64 / (filter_height * filter_width) as f64).round() as u8;
         use image::Luma;
         let new_pixel = Luma([average]);
         new_image.put_pixel(x, y, new_pixel);
