85c56f07822d8dc92240851af9525923fe6149b2
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-05-09 17:18:06 -0400

    started to add generic functions, have to rewrite all of them to be generic

--- examples/pixel_operation.rs
+++ examples/pixel_operation.rs
@@ -8,28 +8,46 @@ use image_processing::window::display_multiple_images;
 use image::ConvertBuffer;
 
 fn main() {
-    let img = image::open("images/england-hampton-court-palace.jpg")
+    // let mut img = image::open("images/england-hampton-court-palace.jpg")
+    //     .expect("couldn't find image at that path")
+    //     .to_luma_alpha();
+    let mut img = image::open("images/england-hampton-court-palace.jpg")
         .expect("couldn't find image at that path")
-        .to_luma_alpha();
-    let next_img = image::open("images/empire-state-building-black-white.jpg")
-        .expect("couldn't find image at that path")
-        .to_luma_alpha();
+        .to_rgb();
+    // let next_img = image::open("images/empire-state-building-black-white.jpg")
+    //     .expect("couldn't find image at that path")
+    //     .to_luma_alpha();
+    // let piecewise_histogram_matching = match_piecewise_linear_histogram_modified(&img, &next_img);
+    // let histogram_matching = histogram_matching(&img, &next_img);
+    let base_img = img.clone();
+    invert_mut(&mut img);
+    let non_mut = invert(&base_img);
+
     let width = 500;
     let height = 500;
-    let piecewise_histogram_matching = match_piecewise_linear_histogram_modified(&img, &next_img);
-    let histogram_matching = histogram_matching(&img, &next_img);
-
     display_multiple_images(
-        &["base", "piecewise linear histogram matching", "target image", "histogram matching"],
+        &["base", "generic invert", "invert non mut"],
         &[
+            &base_img.convert(),
             &img.convert(),
-            &piecewise_histogram_matching.convert(),
-            &next_img.convert(),
-            &histogram_matching.convert(),
+            &non_mut.convert(),
         ],
         width,
         height,
     );
+
+    // display_multiple_images(
+    //     &["base", "piecewise linear histogram matching", "target image", "histogram matching"],
+    //     &[
+    //         &img.convert(),
+    //         &piecewise_histogram_matching.convert(),
+    //         &next_img.convert(),
+    //         &histogram_matching.convert(),
+    //     ],
+    //     width,
+    //     height,
+    // );
+    // brightness(&mut img, 100);
 
     // img.save("images/base.jpg").unwrap();
     // histogram_specification
--- src/filters.rs
+++ src/filters.rs
@@ -1,80 +1,80 @@
-use image::GrayImage;
+// use image::GrayImage;
 
-/// If the filter width or height are not odd in size then they are made into odd lengths
-/// by subtracting 1
-/// If the width or height is less than 3 then the input image is returned without any convolution done
-pub fn box_3x3(image: &GrayImage, filter_width: u32, filter_height: u32) -> GrayImage {
-    let mut new_image = image.clone();
+// /// If the filter width or height are not odd in size then they are made into odd lengths
+// /// by subtracting 1
+// /// If the width or height is less than 3 then the input image is returned without any convolution done
+// pub fn box_3x3(image: &GrayImage, filter_width: u32, filter_height: u32) -> GrayImage {
+//     let mut new_image = image.clone();
 
-    let (image_width, image_height) = image.dimensions();
+//     let (image_width, image_height) = image.dimensions();
 
-    let filter_width: u32 = if filter_width % 2 == 0 {
-        filter_width - 1
-    } else {
-        filter_width
-    };
+//     let filter_width: u32 = if filter_width % 2 == 0 {
+//         filter_width - 1
+//     } else {
+//         filter_width
+//     };
 
-    let filter_height: u32 = if filter_height % 2 == 0 {
-        filter_height - 1
-    } else {
-        filter_height
-    };
+//     let filter_height: u32 = if filter_height % 2 == 0 {
+//         filter_height - 1
+//     } else {
+//         filter_height
+//     };
 
-    if filter_width < 3 || filter_height < 3 {
-        return new_image;
-    }
+//     if filter_width < 3 || filter_height < 3 {
+//         return new_image;
+//     }
 
-    for pixel in image.enumerate_pixels() {
-        let x = pixel.0;
-        let y = pixel.1;
-        let mut sum: u32 = 0;
+//     for pixel in image.enumerate_pixels() {
+//         let x = pixel.0;
+//         let y = pixel.1;
+//         let mut sum: u32 = 0;
 
-        let begin_x: isize = x as isize - (filter_width / 2) as isize;
-        let begin_y: isize = y as isize - (filter_height / 2) as isize;
-        let end_x = begin_x + filter_width as isize;
-        let end_y = begin_y + filter_height as isize;
+//         let begin_x: isize = x as isize - (filter_width / 2) as isize;
+//         let begin_y: isize = y as isize - (filter_height / 2) as isize;
+//         let end_x = begin_x + filter_width as isize;
+//         let end_y = begin_y + filter_height as isize;
 
-        for j in begin_y..end_y {
-            for i in begin_x..end_x {
-                if i < 0 && j < 0 {
-                    // top left
-                    sum += image.get_pixel(0, 0).data[0] as u32;
-                } else if i > (image_width - 1) as isize && j < 0 {
-                    // top right
-                    sum += image.get_pixel(image_width - 1, 0).data[0] as u32;
-                } else if j < 0 {
-                    // top
-                    sum += image.get_pixel(i as u32, 0).data[0] as u32;
-                } else if i < 0 && j > (image_height as isize) - 1 {
-                    // bottom left
-                    sum += image.get_pixel(0, image_height - 1).data[0] as u32;
-                } else if i > image_width as isize - 1 && j > image_height as isize - 1 {
-                    // bottom right
-                    sum += image.get_pixel(image_width - 1, image_height - 1).data[0] as u32;
-                } else if i < 0 {
-                    // left
-                    sum += image.get_pixel(0, j as u32).data[0] as u32;
-                } else if i > image_width as isize - 1 {
-                    // right
-                    sum += image.get_pixel(image_width - 1, j as u32).data[0] as u32;
-                } else if j > image_height as isize - 1 {
-                    // bottom
-                    sum += image.get_pixel(i as u32, image_height - 1).data[0] as u32;
-                } else {
-                    sum += image.get_pixel(i as u32, j as u32).data[0] as u32;
-                }
-            }
-        }
+//         for j in begin_y..end_y {
+//             for i in begin_x..end_x {
+//                 if i < 0 && j < 0 {
+//                     // top left
+//                     sum += image.get_pixel(0, 0).data[0] as u32;
+//                 } else if i > (image_width - 1) as isize && j < 0 {
+//                     // top right
+//                     sum += image.get_pixel(image_width - 1, 0).data[0] as u32;
+//                 } else if j < 0 {
+//                     // top
+//                     sum += image.get_pixel(i as u32, 0).data[0] as u32;
+//                 } else if i < 0 && j > (image_height as isize) - 1 {
+//                     // bottom left
+//                     sum += image.get_pixel(0, image_height - 1).data[0] as u32;
+//                 } else if i > image_width as isize - 1 && j > image_height as isize - 1 {
+//                     // bottom right
+//                     sum += image.get_pixel(image_width - 1, image_height - 1).data[0] as u32;
+//                 } else if i < 0 {
+//                     // left
+//                     sum += image.get_pixel(0, j as u32).data[0] as u32;
+//                 } else if i > image_width as isize - 1 {
+//                     // right
+//                     sum += image.get_pixel(image_width - 1, j as u32).data[0] as u32;
+//                 } else if j > image_height as isize - 1 {
+//                     // bottom
+//                     sum += image.get_pixel(i as u32, image_height - 1).data[0] as u32;
+//                 } else {
+//                     sum += image.get_pixel(i as u32, j as u32).data[0] as u32;
+//                 }
+//             }
+//         }
 
-        let average = (sum as f64 / (filter_height * filter_width) as f64).round() as u8;
-        use image::Luma;
-        let new_pixel = Luma([average]);
-        new_image.put_pixel(x, y, new_pixel);
-    }
+//         let average = (sum as f64 / (filter_height * filter_width) as f64).round() as u8;
+//         use image::Luma;
+//         let new_pixel = Luma([average]);
+//         new_image.put_pixel(x, y, new_pixel);
+//     }
 
-    new_image
-}
-
-// pub fn box_3x3_mut(image: &mut GrayImage) {
-//     let new_image = image.clone();
+//     new_image
 // }
+
+// // pub fn box_3x3_mut(image: &mut GrayImage) {
+// //     let new_image = image.clone();
+// // }
