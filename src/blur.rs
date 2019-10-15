86710542000b2fec50a6d22e78484bbd88b0f13e
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-10-15 18:37:22 -0400

    implemented box blur for color images and implemented transpose for rgba image

--- src/blur.rs
+++ src/blur.rs
@@ -1,5 +1,5 @@
 // use image::RgbaImage;
-use image::{GrayImage, ImageBuffer};
+use image::{RgbaImage, GrayImage, ImageBuffer};
 use image::{GenericImageView};
 
 use std::f32::consts::{E, PI};
@@ -131,7 +131,86 @@ pub fn gaussian_filter_mut(filter: &GaussianKernel, image: &mut GrayImage) {
 // https://fgiesen.wordpress.com/2012/07/30/fast-blurs-1/
 // http://elynxsdk.free.fr/ext-docs/Blur/Fast_box_blur.pdf
 // this filter still isn't complete because it can't take even sized filters
-pub fn box_filter_mut(filter: MeanKernel, image: &mut GrayImage) {
+// pub fn box_filter_mut(filter: MeanKernel, image: &mut GrayImage) {
+//     use crate::matrix_ops::*;
+
+//     let (width, height) = image.dimensions();
+
+//     // want the truncated value of this division, hence not using float
+//     let radius: i32 = filter.size() as i32 / 2;
+
+//     let mut new_image: GrayImage = ImageBuffer::new(width, height);
+
+//     horizontal_blur(radius, image, &mut new_image, width, height);
+//     transpose(&new_image, &mut *image, width as usize, height as usize);
+//     horizontal_blur(radius, image, &mut new_image, height, width);
+//     transpose(&new_image, &mut *image, height as usize, width as usize);
+// } 
+
+// fn horizontal_blur(radius: i32, image: &[u8], blur_image: &mut [u8], width: u32, height: u32) {
+//     let scale = 1.0 / (2.0 * radius as f32 + 1.0);
+
+//     let (width, height) = (width as i32, height as i32);
+//     for y in 0..height {
+//         let mut sum = {
+//             let mut sum: f32 = 0.0;
+//             let begin = 0 - radius;
+//             let end = 0 + radius;
+//             for i in begin..=end {
+//                 if i < 0 {
+//                     unsafe {
+//                         sum += *image.get_unchecked((y * width) as usize) as f32;
+//                     }
+//                 } else if i >= width as i32 {
+//                     unsafe {
+//                         sum += *image.get_unchecked((y * width + width-1) as usize) as f32;
+
+//                     }
+//                 } else {
+//                     unsafe {
+//                         sum += *image.get_unchecked((y * width + i) as usize) as f32;
+//                     }
+//                 }
+//             } 
+//             sum
+//         };
+//         let mut end_pixel = 0.0_f32;
+//         let mut begin_pixel = 0.0_f32;
+//         unsafe {
+//             end_pixel = *image.get_unchecked((y * width + width - 1) as usize) as f32; 
+//             begin_pixel = *image.get_unchecked((y * width) as usize) as f32;
+//         }
+//         for x in 0..width {
+//             let x = x as i32;
+
+//             unsafe {
+//                 let elem = blur_image.get_unchecked_mut((y * width + x) as usize);
+//                 *elem = (sum * scale).round() as u8;
+//             }
+
+//             if x + radius + 1 >= width as i32 && x - radius < 0 {
+//                 unsafe {
+//                     sum += end_pixel - begin_pixel;
+//                 }
+//             } else if x + radius + 1 >= width as i32 {
+//                 unsafe {
+//                     sum += end_pixel - *image.get_unchecked((y * width + x - radius) as usize) as f32;
+//                 }
+//             } else if x - radius < 0 {
+//                 unsafe {
+//                     sum += *image.get_unchecked((y * width + x + radius + 1) as usize) as f32 - begin_pixel;
+//                 }
+//             } else {
+//                 unsafe {
+//                     sum += *image.get_unchecked((y * width + x + radius + 1) as usize) as f32 - *image.get_unchecked((y * width + x - radius) as usize) as f32;
+
+//                 }
+//             }
+//         }
+//     } 
+// }
+
+pub fn box_filter_mut(filter: MeanKernel, image: &mut RgbaImage) {
     use crate::matrix_ops::*;
 
     let (width, height) = image.dimensions();
@@ -139,72 +218,161 @@ pub fn box_filter_mut(filter: MeanKernel, image: &mut GrayImage) {
     // want the truncated value of this division, hence not using float
     let radius: i32 = filter.size() as i32 / 2;
 
-    let mut new_image: GrayImage = ImageBuffer::new(width, height);
+    // let mut new_image: RgbaImage = ImageBuffer::new(width, height);
+    let mut new_image: RgbaImage = ImageBuffer::new(width, height);
+    let mut vertical_image: RgbaImage = ImageBuffer::new(height, width);
+    let mut vertical_image_blur: RgbaImage = ImageBuffer::new(height, width);
 
+    // blur pixels row wise
     horizontal_blur(radius, image, &mut new_image, width, height);
-    transpose(&new_image, &mut *image, width as usize, height as usize);
-    horizontal_blur(radius, image, &mut new_image, height, width);
-    transpose(&new_image, &mut *image, height as usize, width as usize);
-} 
+    transpose(&new_image, &mut vertical_image);
 
+    // blur pixels column wise
+    horizontal_blur(radius, &vertical_image, &mut vertical_image_blur, height, width);
+    transpose(&vertical_image_blur, image);
+} 
+const CHANNEL_COUNT: i32 = 4;
 fn horizontal_blur(radius: i32, image: &[u8], blur_image: &mut [u8], width: u32, height: u32) {
     let scale = 1.0 / (2.0 * radius as f32 + 1.0);
 
     let (width, height) = (width as i32, height as i32);
     for y in 0..height {
-        let mut sum = {
-            let mut sum: f32 = 0.0;
+        let (mut sum_red, mut sum_green, mut sum_blue) = {
+            // let mut sum: f32 = 0.0;
+            let mut sum_red = 0.0_f32;
+            let mut sum_green = 0.0_f32;
+            let mut sum_blue = 0.0_f32;
+
             let begin = 0 - radius;
-            let end = 0 + radius;
+            let end = (0 + radius);
             for i in begin..=end {
                 if i < 0 {
-                    unsafe {
-                        sum += *image.get_unchecked((y * width) as usize) as f32;
-                    }
-                } else if i >= width as i32 {
-                    unsafe {
-                        sum += *image.get_unchecked((y * width + width-1) as usize) as f32;
+                    // unsafe {
+                    //     // sum += *image.get_unchecked((y * width) as usize) as f32;
+                    // }
+                    // sum += *image.get_unchecked((y * width) as usize) as f32;
+                    let pixel_index = (y * width * CHANNEL_COUNT) as usize;
 
-                    }
+                    // dbg!(pixel_index);
+
+                    sum_red += *image.get(pixel_index).unwrap() as f32;
+
+                    sum_green += *image.get(pixel_index + 1).unwrap() as f32;
+
+                    sum_blue += *image.get(pixel_index + 2).unwrap() as f32;
+
+                } else if i >= width as i32 {
+                    // unsafe {
+                    //     // sum += *image.get_unchecked((y * width + width-1) as usize) as f32;
+
+                    // }
+                    // sum += *image.get_unchecked((y * width + width-1) as usize) as f32;
+                    let pixel_index = (y * width * CHANNEL_COUNT + (width - 1) * CHANNEL_COUNT) as usize;
+                    // dbg!(pixel_index);
+                    sum_red += *image.get(pixel_index).unwrap() as f32;
+                    sum_green += *image.get(pixel_index + 1).unwrap() as f32;
+                    sum_blue += *image.get(pixel_index + 2).unwrap() as f32;
+
                 } else {
-                    unsafe {
-                        sum += *image.get_unchecked((y * width + i) as usize) as f32;
-                    }
+                    // unsafe {
+                    //     // sum += *image.get_unchecked((y * width + i) as usize) as f32;
+                    //     let pixel = (y * (width * CHANNEL_COUNT) + i * CHANNEL_COUNT) as usize;
+                    //     sum_red += *image.get_unchecked(pixel) as f32;
+                    //     sum_green += *image.get_unchecked(pixel + 1) as f32;
+                    //     sum_blue += *image.get_unchecked(pixel + 2) as f32;
+                    // }
+                        // sum += *image.get_unchecked((y * width + i) as usize) as f32;
+                    let pixel_index = (y * width * CHANNEL_COUNT + i * CHANNEL_COUNT) as usize;
+                    // dbg!(pixel_index);
+                    sum_red += *image.get(pixel_index).unwrap() as f32;
+                    sum_green += *image.get(pixel_index + 1).unwrap() as f32;
+                    sum_blue += *image.get(pixel_index + 2).unwrap() as f32;
                 }
             } 
-            sum
+            (sum_red, sum_green, sum_blue)
         };
-        let mut end_pixel = 0.0_f32;
-        let mut begin_pixel = 0.0_f32;
-        unsafe {
-            end_pixel = *image.get_unchecked((y * width + width - 1) as usize) as f32; 
-            begin_pixel = *image.get_unchecked((y * width) as usize) as f32;
-        }
+        // let mut end_pixel = 0.0_f32;
+        // let mut begin_pixel = 0.0_f32;
+        // unsafe {
+        //     end_pixel = *image.get_unchecked((y * width + width - 1) as usize) as f32; 
+        //     begin_pixel = *image.get_unchecked((y * width) as usize) as f32;
+        // }
+
+        let begin_index = (y * width * CHANNEL_COUNT) as usize;
+        let begin_pixel_red = *image.get(begin_index).unwrap() as f32;
+        let begin_pixel_green = *image.get(begin_index + 1).unwrap() as f32;
+        let begin_pixel_blue = *image.get(begin_index + 2).unwrap() as f32;
+        // dbg!(begin_index);
+
+        let end_index = (y * width * CHANNEL_COUNT + (width - 1) * CHANNEL_COUNT) as usize;
+        // dbg!(end_index);
+        let end_pixel_red = *image.get(end_index).unwrap() as f32;
+        let end_pixel_green = *image.get(end_index + 1).unwrap() as f32;
+        let end_pixel_blue = *image.get(end_index + 2).unwrap() as f32;
+
         for x in 0..width {
-            let x = x as i32;
 
-            unsafe {
-                let elem = blur_image.get_unchecked_mut((y * width + x) as usize);
-                *elem = (sum * scale).round() as u8;
-            }
+            // unsafe {
+            //     let luma = blur_image.get_unchecked_mut((y * width + x) as usize);
+            //     *luma = (sum * scale).round() as u8;
+            // }
+            let current_pixel = (y * width * CHANNEL_COUNT + x * CHANNEL_COUNT) as usize;
+            // dbg!(current_pixel);
+            let red = blur_image.get_mut(current_pixel).unwrap();
+            *red = (sum_red * scale).round() as u8;
+            // dbg!(*red);
 
+            let green = blur_image.get_mut(current_pixel + 1).unwrap();
+            *green = (sum_green * scale).round() as u8;
+            // dbg!(*green);
+
+            let blue = blur_image.get_mut(current_pixel + 2).unwrap();
+            *blue = (sum_blue * scale).round() as u8;
+            // dbg!(*blue);
+            let alpha = blur_image.get_mut(current_pixel + 3).unwrap();
+            *alpha = 255;
             if x + radius + 1 >= width as i32 && x - radius < 0 {
-                unsafe {
-                    sum += end_pixel - begin_pixel;
-                }
-            } else if x + radius + 1 >= width as i32 {
-                unsafe {
-                    sum += end_pixel - *image.get_unchecked((y * width + x - radius) as usize) as f32;
-                }
-            } else if x - radius < 0 {
-                unsafe {
-                    sum += *image.get_unchecked((y * width + x + radius + 1) as usize) as f32 - begin_pixel;
-                }
-            } else {
-                unsafe {
-                    sum += *image.get_unchecked((y * width + x + radius + 1) as usize) as f32 - *image.get_unchecked((y * width + x - radius) as usize) as f32;
+                // unsafe {
+                //     sum += end_pixel - begin_pixel;
+                // }
+                sum_red += end_pixel_red - begin_pixel_red;
 
-                }
+                sum_green += end_pixel_green - begin_pixel_green;
+
+                sum_blue += end_pixel_blue - begin_pixel_blue;
+
+            } else if x + radius + 1 >= width as i32 {
+                // unsafe {
+                //     sum += end_pixel - *image.get_unchecked((y * width + x - radius) as usize) as f32;
+                // }
+                let pixel_index = (y * width * CHANNEL_COUNT + (x - radius) * CHANNEL_COUNT) as usize;
+                sum_red += end_pixel_red - *image.get(pixel_index).unwrap() as f32;
+                // dbg!(end_pixel_red - *image.get(pixel_index).unwrap() as f32);
+                sum_green += end_pixel_green - *image.get(pixel_index + 1).unwrap() as f32;
+                // dbg!(end_pixel_green - *image.get(pixel_index + 1).unwrap() as f32);
+                sum_blue += end_pixel_blue - *image.get(pixel_index + 2).unwrap() as f32;
+                // dbg!(end_pixel_blue - *image.get(pixel_index + 2).unwrap() as f32);
+                // println!();
+            } else if x - radius < 0 {
+                // unsafe {
+                //     sum += *image.get_unchecked((y * width + x + radius + 1) as usize) as f32 - begin_pixel;
+                // }
+                // sum += *image.get((y * width + x + radius + 1) as usize) as f32 - begin_pixel;
+                let pixel_index = (y * width * CHANNEL_COUNT + (x + radius + 1) * CHANNEL_COUNT) as usize;
+                // dbg!(pixel_index);
+                sum_red += *image.get(pixel_index).unwrap() as f32 - begin_pixel_red;
+                sum_green += *image.get(pixel_index + 1).unwrap() as f32 - begin_pixel_green;
+                sum_blue += *image.get(pixel_index + 2).unwrap() as f32 - begin_pixel_blue;
+            } else {
+                // unsafe {
+                //     sum += *image.get_unchecked((y * width + x + radius + 1) as usize) as f32 - *image.get_unchecked((y * width + x - radius) as usize) as f32;
+                // }
+                // sum += *image.get((y * width + x + radius + 1) as usize) as f32 - *image.get((y * width + x - radius) as usize) as f32;
+                let last_pixel_index = (y * width * CHANNEL_COUNT + (x + radius + 1) * CHANNEL_COUNT) as usize;
+                let first_pixel_index = (y * width * CHANNEL_COUNT + (x - radius) * CHANNEL_COUNT) as usize;
+                sum_red += *image.get(last_pixel_index).unwrap() as f32 - *image.get(first_pixel_index).unwrap() as f32;
+                sum_green += *image.get(last_pixel_index + 1).unwrap() as f32 - *image.get(first_pixel_index + 1).unwrap() as f32;
+                sum_blue += *image.get(last_pixel_index + 2).unwrap() as f32 - *image.get(first_pixel_index + 2).unwrap() as f32;
             }
         }
     } 
--- src/matrix_ops.rs
+++ src/matrix_ops.rs
@@ -1,30 +1,44 @@
-pub fn transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
-    assert!(
-        width * height == buf.len(), 
-        "width * height not equal buf.len(): {} {}", 
-        width * height, 
-        buf.len()
-    );
-    assert!(
-        buf.len() == transpose.len(),
-        "buf length: {}, out_buf length: {}", 
-        buf.len(), 
-        transpose.len()
-    );
+use image::RgbaImage;
+use image::{GenericImage, GenericImageView};
+// pub fn transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
+pub fn transpose(image: &RgbaImage, transpose: &mut RgbaImage)  {
+
+    // assert!(
+    //     width * height * stride == buf.len(), 
+    //     "width * height not equal buf.len(): {} {}", 
+    //     width * height, 
+    //     buf.len()
+    // );
+    // assert!(
+    //     buf.len() == transpose.len(),
+    //     "buf length: {}, out_buf length: {}", 
+    //     buf.len(), 
+    //     transpose.len()
+    // );
 
     let block: usize = 16;
-    for y in (0..height).step_by(block) {
-        for x in (0..width).step_by(block) {
-            for k in y..(y + block).min(height) {
-                for j in x..(x + block).min(width) {
-                    unsafe {
-                        let out_elem = transpose.get_unchecked_mut(j * height + k);
-                        *out_elem = *buf.get_unchecked(k * width + j);
-                    }
-                }
-            }
+    // for (chunk, transpose_chunk) in buf.chunks(block).zip(transpose.chunks(block)) {
+    //     for elem in chunk.iter().zip(transpose_chunk.iter()) {
+
+    //     }
+    // }
+    let (width, height) = image.dimensions();
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = image.get_pixel(x, y);
+            let transpose_pixel = transpose.get_pixel_mut(y, x);
+            *transpose_pixel = *pixel;
         }
     }
+    // for (pixel, transpose_pixel) in image.pixels().zip(transpose.pixels_mut()) {
+
+
+    // }
+  
+}
+const stride: usize = 4;
+fn index(coordinates: (usize, usize), width: usize) -> usize {
+    coordinates.1 * width * stride + coordinates.0 * stride
 }
 
 pub fn faster_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
@@ -83,6 +97,22 @@ mod tests {
                 let first_val = matrix[y * 3 + x];
                 let second_val = matrix_transpose[x * 3 + y];
                 assert!(first_val == second_val, "{} {}", first_val, second_val); 
+            }
+        }
+    }
+
+    #[test]
+    fn test_transpose_on_color_image() {
+        let matrix: [u8; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        let mut matrix_transpose: [u8; 16] = [0; 16];
+        let (width, height) = (2, 2);
+        transpose(&matrix, &mut matrix_transpose, width, height);
+        for y in 0..height {
+            for x in 0..width {
+                let first_val = matrix[y * width + x];
+                let second_val = matrix_transpose[x * height + y];
+                assert!(first_val == second_val, "{} {}", first_val, second_val); 
+
             }
         }
     }
