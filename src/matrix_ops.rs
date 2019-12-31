999dc8e1f3f32354617fdb800e96c3d97a7e05df
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-12-31 15:07:55 -0500

    adding matrix transpose to be used with grayscale images and a test for it

--- src/matrix_ops.rs
+++ src/matrix_ops.rs
@@ -1,27 +1,8 @@
 use image::RgbaImage;
 use image::{GenericImage, GenericImageView};
-// pub fn transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
+// pub fn gray_image_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
 pub fn transpose(image: &RgbaImage, transpose: &mut RgbaImage)  {
 
-    // assert!(
-    //     width * height * stride == buf.len(), 
-    //     "width * height not equal buf.len(): {} {}", 
-    //     width * height, 
-    //     buf.len()
-    // );
-    // assert!(
-    //     buf.len() == transpose.len(),
-    //     "buf length: {}, out_buf length: {}", 
-    //     buf.len(), 
-    //     transpose.len()
-    // );
-
-    let block: usize = 16;
-    // for (chunk, transpose_chunk) in buf.chunks(block).zip(transpose.chunks(block)) {
-    //     for elem in chunk.iter().zip(transpose_chunk.iter()) {
-
-    //     }
-    // }
     let (width, height) = image.dimensions();
     for y in 0..height {
         for x in 0..width {
@@ -30,19 +11,10 @@ pub fn transpose(image: &RgbaImage, transpose: &mut RgbaImage)  {
             *transpose_pixel = *pixel;
         }
     }
-    // for (pixel, transpose_pixel) in image.pixels().zip(transpose.pixels_mut()) {
-
-
-    // }
   
 }
-const stride: usize = 4;
-fn index(coordinates: (usize, usize), width: usize) -> usize {
-    coordinates.1 * width * stride + coordinates.0 * stride
-}
-
-pub fn faster_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
-    assert!(
+pub fn matrix_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
+     assert!(
         width * height == buf.len(), 
         "width * height not equal buf.len(): {} {}", 
         width * height, 
@@ -55,29 +27,9 @@ pub fn faster_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: 
         transpose.len()
     );
 
-    let block: usize = 16;
-    let x_end = width / block;
-    let y_end = height / block;
-
-    for y in (0..y_end).step_by(block) {
-        for x in (0..x_end).step_by(block) {
-            for k in y..y + block {
-                for j in x..x + block {
-                    unsafe {
-                        let out_elem = transpose.get_unchecked_mut(j * height + k);
-                        *out_elem = *buf.get_unchecked(k * width + j);
-                    }
-                }
-            }
-        }
-    }
-    for y in y_end..height {
-        for x in x_end..width {
-            unsafe {
-                let out_elem = transpose.get_unchecked_mut(x * height + y);
-                *out_elem = *buf.get_unchecked(y * width + x);
-            }
-
+    for y in 0..height {
+        for x in 0..width {
+            transpose[x * height + y] = buf[y * width + x]; 
         }
     }
 }
@@ -87,46 +39,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_transpose() {
+    fn test_matrix_transpose() {
         let mut matrix: [u8; 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        let mut matrix_transpose: [u8; 9] = [0; 9];
+        let mut output_matrix: [u8; 9] = [0; 9];
 
-        transpose(&matrix, &mut matrix_transpose, 3, 3);
+        matrix_transpose(&matrix, &mut output_matrix, 3, 3);
         for y in 0..3 {
             for x in 0..3 {
                 let first_val = matrix[y * 3 + x];
-                let second_val = matrix_transpose[x * 3 + y];
-                assert!(first_val == second_val, "{} {}", first_val, second_val); 
-            }
-        }
-    }
-
-    #[test]
-    fn test_transpose_on_color_image() {
-        let matrix: [u8; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-        let mut matrix_transpose: [u8; 16] = [0; 16];
-        let (width, height) = (2, 2);
-        transpose(&matrix, &mut matrix_transpose, width, height);
-        for y in 0..height {
-            for x in 0..width {
-                let first_val = matrix[y * width + x];
-                let second_val = matrix_transpose[x * height + y];
-                assert!(first_val == second_val, "{} {}", first_val, second_val); 
-
-            }
-        }
-    }
-
-    #[test]
-    fn test_faster_transpose() {
-        let mut matrix: [u8; 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        let mut matrix_transpose: [u8; 9] = [0; 9];
-
-        faster_transpose(&matrix, &mut matrix_transpose, 3, 3);
-        for y in 0..3 {
-            for x in 0..3 {
-                let first_val = matrix[y * 3 + x];
-                let second_val = matrix_transpose[x * 3 + y];
+                let second_val = output_matrix[x * 3 + y];
                 assert!(first_val == second_val, "{} {}", first_val, second_val); 
             }
         }
