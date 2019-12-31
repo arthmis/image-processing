620baf2b48b6615e78d4602d92dfc09ba1b78970
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-12-31 15:07:15 -0500

    updating histogram example to handle when user doesn't use display window feature

--- examples/histogram_image.rs
+++ examples/histogram_image.rs
@@ -1,11 +1,17 @@
-use image_processing::histogram::*;
-use image_processing::window::*;
-
+#[cfg(feature = "display-window")]
 fn main() {
+    use image_processing::histogram::*;
+    use image_processing::window::*;
+
     let image = image::open("./images/england-hampton-court-palace.jpg").expect("image not found").to_rgba();
     let histogram = LumaHistogram::from_rgba_image(&image);
     let hist_image = convert_to_image(256, 256, &histogram);
     let (width, height) = (800, 800);
     display_image("histogram image", &hist_image, width, height);
 
+}
+
+#[cfg(not(feature = "display-window"))]
+fn main() {
+    panic!("Displaying images is supported only when the feature 'display window' is enabled.");
 }
