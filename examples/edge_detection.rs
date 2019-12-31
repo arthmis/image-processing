4da11bf217ca3c8a0cc5d2ada6922acfc3a44e02
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-12-31 15:09:24 -0500

    adding a slightly faster sobel edge detector that uses transposing to be more cache friendly

--- examples/edge_detection.rs
+++ examples/edge_detection.rs
@@ -1,34 +1,42 @@
-use image_processing::window::*;
-use image_processing::edge_detection::*;
-use image::ConvertBuffer;
-use image_processing::pixel_ops::threshold_mut;
-use coz;
 
 #[cfg(feature = "display-window")]
 fn main() {
+    use image_processing::window::*;
+    use image_processing::edge_detection::*;
+    use image::ConvertBuffer;
+    use image_processing::pixel_ops::threshold_mut;
+
     let mut image = image::open("./images/england-hampton-court-palace.jpg")
         .expect("image not found")
         .to_luma();
 
-    let (width, height) = (500, 500);
-    let mut x_image = image.clone();
-    let mut y_image = image.clone();
+    let mut image_clone = image.clone();
+    let (width, height) = (800, 800);
+    // let mut x_image = image.clone();
+    // let mut y_image = image.clone();
 
-    sobel_x(&mut x_image);
-    sobel_y(&mut y_image);
-    sobel_mut(&mut image);
+    // sobel_x(&mut x_image);
+    // sobel_y(&mut y_image);
 
     let threshold = 120;
-    threshold_mut(&mut x_image, threshold);
-    threshold_mut(&mut y_image, threshold);
-    threshold_mut(&mut image, threshold);
+    sobel_mut(&mut image, threshold);
+    slower_sobel_mut(&mut image_clone, threshold);
+    // threshold_mut(&mut x_image, threshold);
+    // threshold_mut(&mut y_image, threshold);
 
-    display_multiple_images( 
-        &["sobel", "sobel x ", "sobel y"], 
-        &[&image.convert(), &x_image.convert(), &y_image.convert()], 
+    // display_image("sobel horizontal", &image.convert(), width, height);
+    display_multiple_images(
+        &["sobel regular", "sobel fast"], 
+        &[&image.convert(), &image_clone.convert()], 
         width, 
         height
     );
+    // display_multiple_images( 
+    //     &["sobel", "sobel x ", "sobel y"], 
+    //     &[&image.convert(), &x_image.convert(), &y_image.convert()], 
+    //     width, 
+    //     height
+    // );
 
 }
 
