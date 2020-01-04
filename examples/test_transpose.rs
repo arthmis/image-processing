18fb0652be6d87b303d21202113d87fa2636d1ec
lazypassion <25536767+lazypassion@users.noreply.github.com>
2020-01-03 22:02:56 -0500

    adding generic transpose function that should handle any packed image with any sized stride

--- examples/test_transpose.rs
+++ examples/test_transpose.rs
@@ -1,46 +1,67 @@
 use image_processing::matrix_ops::*;
+use image::{RgbaImage, GrayImage};
+use image::ConvertBuffer;
+use image::ImageBuffer;
 
 fn main() {
 
-    let mut matrix: [u8; 12] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 10, 111];
-    let mut matrix_transpose: [u8; 12] = [0; 12];
-    let mut re_transpose: [u8; 12] = [0; 12];
+    // let mut matrix  = vec![
+    //     0, 1, 2, 255, 3, 4, 5, 255, 6, 7, 8, 255, 
+    //     11, 12, 13, 255, 14, 15, 16, 255, 17, 18, 19, 255
+    // ];
+    // let mut matrix_transpose = vec![0_u8; 24];
 
-    let (width, height) = (4, 3);
-    // transpose(&matrix, &mut matrix_transpose, 4, 3);
-    transpose(&matrix, &mut matrix_transpose, width, height);
-    // dbg!(matrix_transpose);
-    // dbg!(matrix);
-    for y in 0..height {
-        for x in 0..width {
-            let first_val = matrix[y * width + x];
-            let second_val = matrix_transpose[x * height + y];
-            assert!(first_val == second_val, "{} {}", first_val, second_val); 
-        }
-    }
-    for y in 0..height {
-        println!();
-        for x in 0..width {
-            let val = matrix[y * width + x];
-            print!("{} ", val);
-        }
-    }
-    println!();
-    for y in 0..width {
-        println!();
-        for x in 0..height {
-            let val = matrix_transpose[y * height + x];
-            print!("{} ", val);
-        }
-    }
-    println!();
-    transpose(&matrix_transpose, &mut re_transpose, height, width);
-    for y in 0..height {
-        println!();
-        for x in 0..width {
-            let val = matrix[y * width + x];
-            print!("{} ", val);
-        }
-    }
+    // let (width, height) = (3_usize, 2_usize);
+    // let stride: usize = 4;
+    // transpose_generic(&matrix, &mut matrix_transpose, width, height, stride);
+    // // for y in 0..height {
+    // //     for x in 0..width {
+    // //         // let thing: &[u8] = &matrix[(y * width * stride + x)..(x * stride + stride)];
+    // //         // print!("{:?}", thing);
+    // //         // for i in (x * stride)..(x * stride + stride)  {
+    // //         //     // print!("{} ", matrix[y * width * stride + i]);
+    // //         //     // let val = matrix[y * width * stride + i];
+    // //         //     println!("{}", x * height * stride + i);
+    // //         //     // matrix_transpose[x * height * stride + i] = val;
+    // //         // }
+    // //         let min_index = (y * width + x) * stride;
+    // //         let transpose_min_index = (x * height + y) * stride;
+    // //         let matrix_val: &[u8] = &matrix[min_index..(min_index + stride)];
+    // //         let transpose_val: &mut [u8] = &mut matrix_transpose[transpose_min_index..(transpose_min_index + stride)];
+    // //         // print!("{} ", transpose_min_index);
+    // //         // *transpose_val = *matrix_val;
+    // //         for (orig, trans) in matrix_val.iter().zip(transpose_val.iter_mut()) {
+    // //             *trans = *orig;
+    // //         }
+    // //         // print!("{:?}", &matrix[min_index..(min_index + stride)]);
+    // //     }
+    // // }
+    // println!();
+    // println!("{:?}", matrix_transpose);
+    // // transpose(&matrix, &mut matrix_transpose, width, height);
+    let image = image::open("./images/england-hampton-court-palace.jpg")
+        .expect("image not found")
+        .to_rgba();
+    let (width, height) = image.dimensions();
+
+    let gray_image: GrayImage = image.convert();
+    let gray_image_copy = gray_image.clone();
+    let mut third_buffer: GrayImage = ImageBuffer::new(height, width);
+    matrix_transpose(
+        &gray_image, 
+        &mut third_buffer,
+        width as usize, 
+        height as usize,
+    );
+
+    let mut fourth_buffer: GrayImage = ImageBuffer::new(height, width);
+    transpose_generic(
+        &gray_image_copy, 
+        &mut fourth_buffer,
+        width as usize, 
+        height as usize,
+        1, 
+    );
+
 
 }
--- src/matrix_ops.rs
+++ src/matrix_ops.rs
@@ -1,9 +1,9 @@
 use image::RgbaImage;
-use image::{GenericImage, GenericImageView};
-// pub fn gray_image_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
+use image::GrayImage;
 pub fn transpose(image: &RgbaImage, transpose: &mut RgbaImage)  {
 
     let (width, height) = image.dimensions();
+
     for y in 0..height {
         for x in 0..width {
             let pixel = image.get_pixel(x, y);
@@ -12,6 +12,46 @@ pub fn transpose(image: &RgbaImage, transpose: &mut RgbaImage)  {
         }
     }
   
+}
+pub fn transpose_gray(image: &GrayImage, transpose: &mut GrayImage)  {
+
+    assert!(image.len() == transpose.len());
+    let (width, height) = image.dimensions();
+
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = image.get_pixel(x, y);
+            let transpose_pixel = transpose.get_pixel_mut(y, x);
+            *transpose_pixel = *pixel;
+        }
+    }
+  
+}
+pub fn transpose_generic<T: Copy>(image: &[T], out_image: &mut [T], width: usize, height: usize, stride: usize) {
+    assert!(
+        width * height * stride == image.len(),
+        "width * height not equal image.len(): {} {}", 
+        width * height,
+        image.len(),
+    );
+    assert!(
+        image.len() == out_image.len(),
+        "buf length: {}, out_buf length: {}", 
+        image.len(), 
+        out_image.len(),
+    );
+
+    for y in 0..height {
+        for x in 0..width {
+            let image_min_index = (y * width + x) * stride;
+            let trans_min_index = (x * height + y) * stride;
+
+            let image_pixel  = &image[image_min_index..(image_min_index + stride)];
+            let transpose_pixel = &mut out_image[trans_min_index..(trans_min_index + stride)];
+
+            transpose_pixel.copy_from_slice(image_pixel);
+        }
+    }
 }
 pub fn matrix_transpose(buf: &[u8], transpose: &mut [u8], width: usize, height: usize)  {
      assert!(
@@ -40,10 +80,25 @@ mod tests {
 
     #[test]
     fn test_matrix_transpose() {
-        let mut matrix: [u8; 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let matrix: [u8; 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
         let mut output_matrix: [u8; 9] = [0; 9];
 
         matrix_transpose(&matrix, &mut output_matrix, 3, 3);
+        for y in 0..3 {
+            for x in 0..3 {
+                let first_val = matrix[y * 3 + x];
+                let second_val = output_matrix[x * 3 + y];
+                assert!(first_val == second_val, "{} {}", first_val, second_val); 
+            }
+        }
+    }
+    #[test]
+    fn test_matrix_transpose_generic() {
+        let matrix: [u8; 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let mut output_matrix: [u8; 9] = [0; 9];
+
+        let stride = 1;
+        transpose_generic(&matrix, &mut output_matrix, 3, 3, stride);
         for y in 0..3 {
             for x in 0..3 {
                 let first_val = matrix[y * 3 + x];
